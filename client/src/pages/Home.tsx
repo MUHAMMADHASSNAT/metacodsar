c546aa7473071b510aa019c52fb62ae295140667
Arfan Abid <iffionex111@gmail.com>
2025-10-18 21:18:21 +0500

    fix: deployment issue

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import { ArrowRight, Code, Smartphone, Cloud, Users } from 'lucide-react';
+import { ArrowRight, Code, Smartphone, Cloud } from 'lucide-react';
 
 const Home = () => {
   return (
